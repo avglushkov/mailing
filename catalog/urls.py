c6b9c450bad9c76a489600c8dc62c9eb02bb4090
Glushkov Alex <avgl@mail.ru>
2024-06-23 20:52:10 +0300

    Final commit for HW 21.1 FBV и CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
 from catalog.apps import NewappConfig
-from catalog.views import home, contacts, product
+from catalog.views import ProductListView, ProductDetailView, ContactCreateView, BlogListView, BlogCreateView, \
+    BlogDetailView, BlogUpdateView, BlogDeleteView
 
 app_name = NewappConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts'),
-    path('products/<int:pk>', product, name='product'),
+    path('', ProductListView.as_view(), name='home'),
+    path('blog/', BlogListView.as_view(), name='blog_list'),
+    path('contacts/', ContactCreateView.as_view(), name='contacts'),
+    path('blog/blog_form', BlogCreateView.as_view(), name='blog_form'),
+    path('blog/blog_form/<int:pk>', BlogUpdateView.as_view(), name='blog_form'),
+    path('products/<int:pk>', ProductDetailView.as_view(), name='product'),
+    path('blog/<int:pk>', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/delete/<int:pk>', BlogDeleteView.as_view(), name='blog_confirm_delete'),
 ]
